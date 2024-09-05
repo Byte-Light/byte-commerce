f5687eaabdc80d473b713a1ab0601c317a3294ad
SheikhULAB <tosibalaptop86@gmail.com>
2024-09-05 09:42:00 +0600

    check auth

---
+++ client/src/pages/unauth-page/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UnauthPage = () => {
+    return (
+        <div>
+            <h1>You dont have access.</h1>
+        </div>
+    );
+};
+
+export default UnauthPage;
