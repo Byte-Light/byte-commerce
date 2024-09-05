9d40f33073d6c576ac6056cdcef882b2b628c8cb
SheikhULAB <tosibalaptop86@gmail.com>
2024-09-05 22:21:46 +0600

    login and register complete

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const cookieParser = require('cookie-parser')
 const cors = require('cors')
+const authRouter = require('./routes/auth/auth-routes')
 
 mongoose.connect('mongodb+srv://byte-commerce:byte-commerce@cluster0.t4xkx.mongodb.net/').then(() => console.log('Mongodb connected')).catch((error) => console.log(error));
 
@@ -10,20 +11,22 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
     cors({
-        origin: 'http://localhost:5173/',
-        methods: ['GET','POST','DELETE','PUT'],
-        allowedHeaders: [
-            'Content-Type',
-            'Authorization',
-            'Cache-Control',
-            'Expires',
-            'Pragma'
-        ],
-        Credential: true
+      origin: 'http://localhost:5173', // The front-end origin
+      methods: ['GET', 'POST', 'DELETE', 'PUT'],
+      allowedHeaders: [
+        'Content-Type',
+        'Authorization',
+        'Cache-Control',
+        'Expires',
+        'Pragma'
+      ],
+      credentials: true, // This allows the browser to send cookies with requests
     })
-);
+  );
+  
 
 app.use(cookieParser());
 app.use(express.json());
+app.use('/api/auth', authRouter);
 
 app.listen(PORT, () => console.log(`server is running on port ${PORT}`))
