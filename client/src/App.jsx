d05008fc1ce0c22b57e1c2eb3c5453887d81fdc0
SheikhULAB <tosibalaptop86@gmail.com>
2024-09-05 11:40:26 +0600

    register page design

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -19,12 +19,9 @@ import CheckAuth from './components/common/check-auth'
 import UnauthPage from './pages/unauth-page'
 
 function App() {
-  const isAuthenticated = true;
-  const user = {
-    name: 'yeamin',
-    role: 'admin'
-  }
-
+  const isAuthenticated = false;
+  const user = null;
+  
   return (
     <div className='flex flex-col overflow-hidden bg-white'>
 
